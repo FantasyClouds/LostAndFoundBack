78e1079eb450da56499d3fb145038cfb101b2a0e
FantasyClouds <3386483787@qq.com>
2025-05-14 10:01:12 +0800

    添加公网ip的跨域访问

--- src/main/java/com/example/test1/config/CorsConfig.java
+++ src/main/java/com/example/test1/config/CorsConfig.java
@@ -7,7 +7,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(org.springframework.web.servlet.config.annotation.CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080") // 明确列出允许的源
+                .allowedOrigins("http://localhost:8080", "http://47.111.151.198:8080") // 明确列出允许的源
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
