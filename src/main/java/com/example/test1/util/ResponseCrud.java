3f2932435a56c0d9fe7e7e92157ed7a46a328cce
FantasyClouds <3386483787@qq.com>
2025-05-27 21:37:40 +0800

    Amis表格样例（第一阶段修正）

--- src/main/java/com/example/test1/util/ResponseCrud.java
+++ src/main/java/com/example/test1/util/ResponseCrud.java
@@ -23,29 +23,4 @@ public class ResponseCrud<T> {
     public void setRows(List<T> rows) {
         this.rows = rows;
     }
-}package com.example.test1.util;
-
-import org.springframework.stereotype.Repository;
-import java.util.List;
-
-@Repository
-public class ResponseCrud<T> {
-    public List<T> rows;
-    public int count;
-
-    public Integer getCount() {
-        return count;
-    }
-
-    public void setCount(Integer count) {
-        this.count = count;
-    }
-
-    public List<T> getRows() {
-        return rows;
-    }
-
-    public void setRows(List<T> rows) {
-        this.rows = rows;
-    }
 }
